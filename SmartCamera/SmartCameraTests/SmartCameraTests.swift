64f50e4b7aef8ddab5acc8df6fd7fbc99d25e557
Daniel Kwan <48447811+ddanielkwan@users.noreply.github.com>
2020-09-13 10:23:39 -0400

    Add files via upload

---
+++ SmartCamera/SmartCameraTests/SmartCameraTests.swift
@@ -0,0 +1,36 @@
+//
+//  SmartCameraTests.swift
+//  SmartCameraTests
+//
+//  Created by Harshul Mehta on 2020-09-02.
+//  Copyright © 2020 Harshul Mehta. All rights reserved.
+//
+
+import XCTest
+@testable import SmartCamera
+
+class SmartCameraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
